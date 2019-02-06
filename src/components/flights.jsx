082f5755c272880106632100538f22d652cce145
boldizsarBalogh <bal.boldizsar@gmail.com>
2019-02-06 18:16:55 +0100

    added a simple table to visualize the backend data

--- src/components/flights.jsx
+++ src/components/flights.jsx
@@ -1,4 +1,8 @@
 import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.css'
+import Flight from "./flight";
+
+
 
 class Flights extends Component {
 
@@ -28,11 +32,31 @@ class Flights extends Component {
 
     render() {
         return (
+
             <div>
-            <span>{JSON.stringify(this.state.response)}</span>
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">Company</th>
+                        <th scope="col">From</th>
+                        <th scope="col">Arrive at</th>
+                        <th scope="col">Date</th>
+                        <th scope="col">Departure</th>
+                        <th scope="col">Arrival</th>
+                        <th scope="col">Class</th>
+                        <th scope="col">Price($)</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.response.map(item => <Flight company={item.company} startTown={item.startTown} arriveTown={item.arriveTown} date={item.date} startTime={item.startTime} arriveTime={item.arriveTime} confort={item.confort} price={item.price}/>)}
+                    </tbody>
+                </table>
             </div>
         );
     }
 }
+
+
+
 
 export default Flights;
