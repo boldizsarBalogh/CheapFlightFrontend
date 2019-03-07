e167a2f851a8137097c38b99499296ac412e1fb2
boldizsarBalogh <bal.boldizsar@gmail.com>
2019-03-07 12:18:48 +0100

    started implementing auth0 authentication

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 import Devwarning from "./components/devwarning";
 import Search from "./components/search";
 import Flights from "./components/flights";
+import auth0 from 'auth0-js/dist/auth0.min'
 
 
 class App extends Component {
+
 
     constructor(props){
         super(props);
@@ -18,8 +20,86 @@ class App extends Component {
             response : [],
             fromCity: 'Brussels',
             toCity: 'Budapest',
-            cities: []
+            cities: [],
+            idToken: '',
+            accessToken: '',
+            expiresAt: '',
+            webAuth : new auth0.WebAuth({
+                domain: 'zsivany.eu.auth0.com',
+                clientID: 's1vJBtSpTOn1YUibX5fDF8Qp3MYB6-Lg',
+                responseType: 'token id_token',
+                scope: 'admin:todos',
+                audience: 'https://quickstarts/api',
+                redirectUri: window.location.href
+            }),
+            loginStatus : document.querySelector('.container h4'),
+            loginView : document.getElementById('login-view'),
+            homeView : document.getElementById('home-view'),
+            homeViewBtn : document.getElementById('btn-home-view'),
+            logoutBtn : document.getElementById('btn-logout'),
+            loginBtn : document.getElementById('btn-login')
 
+        }
+    }
+    localLogin(authResult) {
+        // Set isLoggedIn flag in localStorage
+        localStorage.setItem('isLoggedIn', 'true');
+        // Set the time that the access token will expire at
+        this.state.expiresAt = JSON.stringify(
+            authResult.expiresIn * 1000 + new Date().getTime()
+        );
+        this.state.accessToken = authResult.accessToken;
+        localStorage.setItem("accessToken", this.state.accessToken);
+        this.state.idToken = authResult.idToken;
+        localStorage.setItem("idToken", this.state.idToken);
+    }
+    logout() {
+        // Remove isLoggedIn flag from localStorage
+        localStorage.clear();
+        // Remove tokens and expiry time
+        this.state.accessToken = '';
+        this.state.idToken = '';
+        this.state.expiresAt = 0;
+        this.displayButtons();
+    }
+    isAuthenticated() {
+        // Check whether the current time is past the
+        // Access Token's expiry time
+        let expiration = parseInt(this.state.expiresAt) || 0;
+        return localStorage.getItem('isLoggedIn') === 'true' && new Date().getTime() < expiration;
+    }
+    displayButtons() {
+        if (this.isAuthenticated()) {
+            this.state.loginBtn.style.display = 'none';
+            this.state.logoutBtn.style.display = 'inline-block';
+            this.state.loginStatus.innerHTML = 'You are logged in!';
+        } else {
+            this.state.loginBtn.style.display = 'inline-block';
+            this.state.logoutBtn.style.display = 'none';
+            this.state.loginStatus.innerHTML =
+                'You are not logged in! Please log in to continue.';
+        }
+    }
+    handleAuthentication() {
+        try {
+            this.state.webAuth.parseHash(function(err, authResult){
+                console.log(authResult.toString());
+                if (authResult && authResult.accessToken && authResult.idToken) {
+                    window.location.hash = '';
+                    this.state.localLogin(authResult);
+                    this.state.loginBtn.style.display = 'none';
+                    this.state.homeView.style.display = 'inline-block';
+                } else if (err) {
+                    this.state.homeView.style.display = 'inline-block';
+                    console.log(err);
+                    alert(
+                        'Error: ' + err.error + '. Check the console for further details.'
+                    );
+                }
+                this.displayButtons();
+            });
+        }catch(TypeError){
+            console.log("not yet authenticated")
         }
     }
 
@@ -27,7 +107,11 @@ class App extends Component {
         this.getCities();
     }
     getCities() {
-        fetch("http://localhost:8000/cities")
+        fetch("http://localhost:8000/cities",{
+            headers: new Headers({
+                'Authorization' : 'Bearer ' + localStorage.getItem("accessToken")
+            })
+        })
             .then(result => result.json())
             .then((res) => {
                 this.setState({cities : res})
