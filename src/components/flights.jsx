4f24be3c7405c41e3d7eb2221a918dc6cd5caca5
boldizsarBalogh <bal.boldizsar@gmail.com>
2019-05-22 14:45:23 +0200

    fixed an authentication bug and renamed some columns in the table

--- src/components/flights.jsx
+++ src/components/flights.jsx
@@ -18,29 +18,26 @@ class Flights extends Component {
 
 
     render() {
-        if (this.props.flights === []) {
+        if (this.props.flights.status === 401) {
+            alert("status code 401 not authorized!!");
             return <span></span>
         }
+        if (this.props.flights.length > 0) {
+             return (
 
-        if(this.props.flights.status === 401){
-            alert("not authorized!!");
-            return <span></span>
-        }
-
-        return (
-
-            <div>
-                <button className="btn btn-success btn-block" type="button" data-toggle="collapse" data-target="#collapseExample"
-                        aria-expanded="false" aria-controls="collapseExample">
-                  ˇˇˇ Flight          search              results ˇˇˇ
-                </button>
-                <div className="collapse" id="collapseExample">
+                <div>
+                    <button className="btn btn-success btn-block" type="button" data-toggle="collapse"
+                            data-target="#collapseExample"
+                            aria-expanded="false" aria-controls="collapseExample">
+                        ˇˇˇ Flight search results ˇˇˇ
+                    </button>
+                    <div className="collapse" id="collapseExample">
                         <table className="table">
                             <thead>
                             <tr>
                                 <th scope="col">Company</th>
-                                <th scope="col">From</th>
-                                <th scope="col">Arrive at</th>
+                                <th scope="col">Origin</th>
+                                <th scope="col">Destination</th>
                                 <th scope="col">Date</th>
                                 <th scope="col">Departure</th>
                                 <th scope="col">Arrival</th>
@@ -48,14 +45,21 @@ class Flights extends Component {
                                 <th scope="col">Price($)</th>
                             </tr>
                             </thead>
-                              <tbody>
-                              {this.props.flights.map(item => <Flight company={item.company} startTown={item.startTown} arriveTown={item.arriveTown} date={item.date} startTime={item.startTime} arriveTime={item.arriveTime} confort={item.confort} price={item.price}/>)}
-                              </tbody>
+                            <tbody>
+                            {this.props.flights.map(item => <Flight company={item.company} startTown={item.startTown}
+                                                                    arriveTown={item.arriveTown} date={item.date}
+                                                                    startTime={item.startTime}
+                                                                    arriveTime={item.arriveTime} confort={item.confort}
+                                                                    price={item.price}/>)}
+                            </tbody>
                         </table>
-                </div>
+                    </div>
 
-            </div>
-        );
+                </div>
+            );
+        } else {
+            return <span></span>
+        }
     }
 }
 
